717ad34fb6a3f0cb2e032217a903a066d3a2f455
Kudhz <Alluka177@gmail.com>
2025-02-12 02:11:39 +0800

    update

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <h1 class="d-flex justify-content-center">JAMET E-KINERJA BOT</h1>
+    <h1 class="d-flex justify-content-center mt-10">JAMET E-KINERJA BOT</h1>
     <form id="form" action="process.php" method="post" onsubmit="return handleSubmit(event)">
     <div class="container w-50 p-3">
             <strong>Auto Ekinerja:</strong><br>
