e7f78f7975a0f48db86071c3a1194e3ab8c5ee22
Kudhz <Alluka177@gmail.com>
2025-02-12 00:51:05 +0800

    update

--- index.php
+++ index.php
@@ -28,10 +28,10 @@
             <div class="malert alert-danger message" id="error-message"></div>
             ID: <input type="text" name="id"  class="form-control"id="id" required><br>
         <!-- Key: <input type="text" name="key2" class="form-control" id="key2" required><br> -->
-        Value Realisasi: <textarea name="value" class="form-control" id="value" cols="50" rows="10"></textarea><br>
-        <!-- Value: <textarea name="value" class="form-control" id="value" cols="50" rows="10">
+        <!-- Value Realisasi: <textarea name="value" class="form-control" id="value" cols="50" rows="10"></textarea><br> -->
+        Value: <textarea name="value" class="form-control" id="value" cols="50" rows="10">
 Menyusun Laporan kegiatan performance check peralatan listrik yang digunakan sebagai dasar pelaksanaan koordinasi dengan unit kerja
-Menyampaikan Laporan Dokumen Laporan kegiatan performance check peralatan listrik kepada pimpinan</textarea><br> -->
+Menyampaikan Laporan Dokumen Laporan kegiatan performance check peralatan listrik kepada pimpinan</textarea><br>
         <input type="submit" class="btn btn-primary" value="Send">
     </div>
     </form>
