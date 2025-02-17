c89e8a9775c6a838d477238ca1b6c76dae893f81
Kudhz <Alluka177@gmail.com>
2025-02-17 22:48:37 +0800

    update login new

--- login.php
+++ login.php
@@ -192,6 +192,8 @@ echo "CAPTCHA Source: $captchaSrc \n";
 echo "CAPTCHA1 Source: $captchaSrc1 \n";
 echo "cookie : $cookieString \n";
 echo "cokkie1 : $cookieString1 \n";
+echo "response : $response \n";
+echo "response1 : $response1 \n";
 ?>
 
 <div class="authentication-bg min-vh-100">
